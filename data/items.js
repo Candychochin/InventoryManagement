dd556c294e3080a156210ef99bc977d6f17e0615
CandyChoChin <toetetaunglin.88@gmail.com>
2026-05-08 11:21:49 +0630

    attech js

---
+++ data/items.js
@@ -0,0 +1,15 @@
+let items = [
+    {
+        id: 1,
+        name: 'Apple',
+        image: 'https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg' ,
+        price: 400,
+        quantity: 2,
+        totalPrice: null
+    } ,
+    
+
+
+
+
+]
---
+++ js/index.js
@@ -0,0 +1,40 @@
+let headTotalPrice = document.getElementById('totalPrice');
+
+// Upper Part is for general
+
+let searchBar = document.getElementById('searchBar');
+
+let addItemPopupBtn = document.getElementById('addBtn');
+
+let addItemPopupClose = document.getElementById('addItemClose');
+
+let addItemName = document.getElementById('addItemName');
+
+let addImageUpload = document.getElementById('imageUpload');
+
+let addPrice = document.getElementById('addPrice');
+
+let addQuantity = document.getElementById('addQuantity');
+
+let addItemConfirmBtn = document.getElementById('addItemBtn')
+
+// Upper Part is for Add Item
+
+let editItemPopupClose = document.getElementById('addItemClose');
+
+let editItemName = document.getElementById('editItemName');
+
+let editImageUpload = document.getElementById('editImageUpload');
+
+let editPrice = document.getElementById('editPrice');
+
+let editQuantity = document.getElementById('editQuantity');
+
+let editConfirmBtn = document.getElementById('confirmEditBtn');
+
+// Upper Part is for Edit Item
+
+let editPopupBtn = document.querySelectorAll('.edit-btn');
+
+let deleteBtn = document.querySelectorAll('.remove-btn');
+
